08ee9e74741cea494d7306df4d5d292c990c3779
EbinezerSamuel <ebhisam4@gmail.com>
2024-05-04 01:01:43 +0530

    updated css

--- formjavascript.js
+++ formjavascript.js
@@ -2,20 +2,30 @@ function formSubmit() {
     console.log("hello world!");
     let form = document.getElementById('myForm');
     let name = form.txtfname.value;
-    console.log(name);
+   console.log(name);
     let mname = form.txtmname.value;
     console.log(mname);
     let laname = form.txtlname.value;
-    console.log(laname);
+   console.log(laname);
     let gend = form.gender.value;
     console.log(gend);
     let phnum = form.number.value;
     console.log(phnum);
     let foodItem = form.food.value;
-    console.log(foodItem);
-   getvalue();
+   console.log(foodItem);
+   let res=getvalue();
     let text = form.pwd.value;
     console.log(text);
+
+     document.getElementById('first').innerText="Name="+name+" "+mname+" "+laname;
+     document.getElementById('gendertag').innerText="Gender="+gend;
+     document.getElementById('telno').innerText="Phone Number="+phnum;
+     document.getElementById('foodings').innerText="Food Items="+foodItem;
+     document.getElementById('juice').innerText="Drinks="+res;
+     document.getElementById('hide').innerText="Hidden secret="+text;
+                                               
+                                             
+                            
 }
 function getvalue()
 {
@@ -30,5 +40,5 @@ function getvalue()
             result+=checkBoxes[i].value+",";
         }
     }
-    console.log("You have selected :"+result);
+    return result;
     }
